b630b24489ca293f420f8488649bf0cdbd6a7c5a
Balthazar Delvaux <delvaux.balthazar@gmail.com>
2020-12-16 03:22:59 +0100

    Added footer

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -12,6 +12,11 @@ export default function Layout ({
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             {children}
+            <footer>
+                <div className="text-center bg-gray-900 py-6">
+                    <p className="text-gray-500">Powered by <a rel="noopener noreferrer" href="https://www.themoviedb.org/">TheMovieDB</a></p>
+                </div>
+            </footer>
         </>
     );
 }
