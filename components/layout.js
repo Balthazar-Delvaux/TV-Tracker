e7f410fbeefa17637551d0131917a5cb8d2ca43b
Balthazar Delvaux <delvaux.balthazar@gmail.com>
2020-12-18 23:52:34 +0100

    Moved navbar into Layout

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,15 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Layout ({
     children,
-    title = 'TV Tracker'
+    title = `TV Tracker`,
+    navbarFixed = false
 }) {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <>
             <Head>
@@ -11,6 +17,74 @@ export default function Layout ({
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
+            <nav className={`flex ${navbarFixed ? `fixed z-10` : `static`} w-full items-center justify-between px-6 h-16 bg-gray-800 bg-opacity-20 text-gray-800`}>
+
+                {/* Left part of the Navbar */}
+                <div className="flex items-stretch">
+
+                    {/* SideBar Toggle */}
+                    <button
+                        className="px-3 py-2 sm:hidden"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        <svg
+                            className="fill-current h-4 w-4"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title>Menu</title>
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                        </svg>
+                    </button>
+
+                    {/* Logo */}
+                    <div className="mx-4">
+                        <h1 className="text-xl font-semibold">TV Tracker</h1>
+                    </div>
+
+                    {/* Side Menu Drawer for small screens / Buttons in Navbar for large screens */}
+                    <div
+                        className={`transform sm:transform-none top-0 left-0 w-64 sm:w-auto h-full sm:h-auto bg-white sm:bg-transparent fixed sm:static overflow-auto sm:overflow-hidden ease-in-out transition-all sm:transition-none sm:flex duration-300 z-30 ${isOpen ? `translate-x-0` : `-translate-x-full`}`}
+                    >
+                        <Link href="/">
+                            <a className="block sm:mx-4">Movies</a>
+                        </Link>
+                        <Link href="/">
+                            <a className="block sm:mx-4">TV Shows</a>
+                        </Link>
+                    </div>
+
+                    {/* Opaque Layout for Side Menu Drawer */}
+                    {isOpen
+                        ? <div className="fixed inset-0 bg-black opacity-50 z-20 h-full overflow-auto" onClick={() => setIsOpen(!isOpen)} ></div>
+                        : null}
+
+                </div>
+
+                {/* Search and Login Buttons */}
+                <div className="d-flex items-stretch">
+
+                    {/* Search Button */}
+                    <div className="inline-block mx-2 mt-1">
+                        <Image
+                            src="/assets/search-outline.svg"
+                            height={32}
+                            width={32}
+                        />
+                    </div>
+                    {/* Login Button */}
+                    <Link href="/login">
+                        <div className="inline-block mx-2 mt-1">
+                            <Image
+                                src="/assets/person-circle-outline.svg"
+                                height={32}
+                                width={32}
+                            />
+                        </div>
+                    </Link>
+                </div>
+
+            </nav>
             {children}
             <footer>
                 <div className="text-center bg-gray-900 py-6">
--- pages/index.js
+++ pages/index.js
@@ -1,95 +1,18 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
-
 import Jumbotron from '../components/header/jumbotron';
 import Layout from '../components/layout';
 import TrendingShowsSection from '../components/main/TrendingShowsSection';
 
 export default function Home ({ trendingShows }) {
-    const [isOpen, setIsOpen] = useState(false);
-
     return (
-        <Layout>
+        <Layout navbarFixed={true}>
             {/* Header */}
             <header className="h-screen text-gray-200">
-
-                {/* Navbar */}
-                <nav className="flex fixed w-full items-center justify-between px-6 h-16 bg-gray-800 bg-opacity-20 text-gray-800 z-10">
-
-                    {/* Left part of the Navbar */}
-                    <div className="flex items-stretch">
-
-                        {/* SideBar Toggle */}
-                        <button
-                            className="px-3 py-2 sm:hidden"
-                            onClick={() => setIsOpen(!isOpen)}
-                        >
-                            <svg
-                                className="fill-current h-4 w-4"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <title>Menu</title>
-                                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                            </svg>
-                        </button>
-
-                        {/* Logo */}
-                        <div className="mx-4">
-                            <h1 className="text-xl font-semibold">TV Tracker</h1>
-                        </div>
-
-                        {/* Side Menu Drawer for small screens / Buttons in Navbar for large screens */}
-                        <div
-                            className={`transform sm:transform-none top-0 left-0 w-64 sm:w-auto h-full sm:h-auto bg-white sm:bg-transparent fixed sm:static overflow-auto sm:overflow-hidden ease-in-out transition-all sm:transition-none sm:flex duration-300 z-30 ${isOpen ? `translate-x-0` : `-translate-x-full`}`}
-                        >
-                            <Link href="/">
-                                <a className="block sm:mx-4">Movies</a>
-                            </Link>
-                            <Link href="/">
-                                <a className="block sm:mx-4">TV Shows</a>
-                            </Link>
-                        </div>
-
-                        {/* Opaque Layout for Side Menu Drawer */}
-                        {isOpen
-                            ? <div className="fixed inset-0 bg-black opacity-50 z-20 h-full overflow-auto" onClick={() => setIsOpen(!isOpen)} ></div>
-                            : null}
-
-                    </div>
-
-                    {/* Search and Login Buttons */}
-                    <div className="d-flex items-stretch">
-
-                        {/* Search Button */}
-                        <div className="inline-block mx-2 mt-1">
-                            <Image
-                                src="/assets/search-outline.svg"
-                                height={32}
-                                width={32}
-                            />
-                        </div>
-                        {/* Login Button */}
-                        <Link href="/login">
-                            <div className="inline-block mx-2 mt-1">
-                                <Image
-                                    src="/assets/person-circle-outline.svg"
-                                    height={32}
-                                    width={32}
-                                />
-                            </div>
-                        </Link>
-                    </div>
-
-                </nav>
 
                 {/* Jumbotron */}
                 <div className="relative z-0 w-full h-full">
                     <Jumbotron props={trendingShows}/>
                 </div>
             </header>
-
             <TrendingShowsSection props={trendingShows}/>
 
         </Layout>
